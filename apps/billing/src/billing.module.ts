c7ac218034da7a45b62ab64a5b81e354ce0abd3a
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2023-05-28 16:28:06 -0300

    [feat]: rabbitmq config

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as joi from 'joi';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
+import { RmqModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        RABBIT_MQ_URI: joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: joi.string().required(),
+      }),
+    }),
+    RmqModule,
+  ],
   controllers: [BillingController],
   providers: [BillingService],
 })
--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -5,9 +5,10 @@ import * as joi from 'joi';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, RmqModule } from '@app/common';
 import { OrdersRepository } from './orders.repository';
 import { Order, OrderSchema } from './schemas/order.schema';
+import { BILLING_SERVICE } from './constants/service';
 
 @Module({
   imports: [
@@ -21,6 +22,9 @@ import { Order, OrderSchema } from './schemas/order.schema';
     }),
     DatabaseModule,
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    RmqModule.register({
+      name: BILLING_SERVICE,
+    }),
   ],
   controllers: [OrdersController],
   providers: [OrdersService, OrdersRepository],
