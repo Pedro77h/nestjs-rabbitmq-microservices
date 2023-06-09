e09645553cba4f913b8318edb0b1921424368293
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2023-06-08 22:20:09 -0300

    create users module

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DatabaseModule } from '@app/common';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
